7bf22fa33308f2a6a2d7306df0ae8e1663bdd227
Khalid Jebbari <khalid.jebbari@gmail.com>
2013-09-09 14:13:42 +0200

    Secont part of 'Starting out' chapter (about lists)

--- 2_starting_out/baby.hs
+++ 2_starting_out/baby.hs
@@ -17,3 +17,33 @@ doubleSmallNumber' x = if x > 100
 -- yo is not a variable, but a function without parameter !
 yo = "Yo !"
 -- yo = 4 -- Fail because definitions are immutable
+
+
+
+---------------------------------
+-- Chapter An intro to lists
+---------------------------------
+
+-- Lists are homogeneous array-like data structures
+lostNumbers = [1,5,9,13,26,49]
+
+-- ++ concatenates 2 lists
+one2four = [1,2] ++ [3,4] -- => [1,2,3,4]
+
+-- Strings are just lists of characters
+hello = ['h', 'e', 'l', 'l', 'o'] -- => "hello" is just syntactic sugar
+
+-- : prepend to the beginning
+aSmallCat = 'A':" SMALL CAT"
+-- single char strings are single quoted, multi chars strings are double quoted
+
+-- : and ++ can be combined
+one2five = 1:[2,3] ++ [4,5]
+
+-- !! retrives element at specified index
+fourthLetter = aSmallCat !! 3 -- => 'M'
+
+-- !! works on a list of lists too (and presumably anything that works on lists)
+lyst = [[1], [2]]
+lyst1 = lyst !! 0 -- => [1]
+lyst11 = (lyst !! 0) !! 0 -- => 1
