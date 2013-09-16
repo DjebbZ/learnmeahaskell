badcee1cf07c1d8b80f31caca33fca857733b619
Khalid Jebbari <khalid.jebbari@gmail.com>
2013-09-16 11:37:59 +0200

    A bit more about Typeclasses

---
+++ 3_types_and_typeclasses/code.hs
@@ -0,0 +1,18 @@
+--------------------
+-- Believe the type
+--------------------
+
+-- :t x displays the type of x
+-- :t 'A' => 'A' :: Char
+-- :t True => True :: Bool
+-- :t take => take :: Int -> [a] -> [a]
+
+
+
+--------------------
+-- Typeclasses 101
+--------------------
+
+-- operators are functions, infixed by default because they only have special chars
+-- to use an operator as prefix function, surround it with parentheses
+-- 1 + 1 == (+) 1 1
