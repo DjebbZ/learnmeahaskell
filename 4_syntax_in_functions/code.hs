bab128b805061208d512a4a619c4717ef486bce3
Khalid Jebbari <khalid.jebbari@gmail.com>
2013-09-16 11:38:30 +0200

    Pattern matching in functions

---
+++ 4_syntax_in_functions/code.hs
@@ -0,0 +1,23 @@
+--------------------
+-- Pattern matching
+--------------------
+
+head' :: [a] -> a
+head' [] = error "No empty list please"
+head' (x:_) = x
+
+
+
+--------------------
+-- Where!?
+--------------------
+
+percentOf :: (RealFloat x) => x -> x -> String
+x `percentOf` y
+    | p == 0    = "Well, O%."
+    | p < 50    = "Less than a half"
+    | p == 50   = "Gand-Half !"
+    | p < 100   = "More that a half"
+    | p == 100  = "Full !"
+    | otherwise = "Too much, man."
+    where p = x / y * 100
